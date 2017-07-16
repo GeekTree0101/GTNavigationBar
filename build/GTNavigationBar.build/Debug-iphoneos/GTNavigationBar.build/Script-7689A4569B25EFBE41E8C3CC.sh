9cfe6349eaab1ff6b7ab4c313fa7bcfa0cfe39b9
GeekTree0101 <h2s1880@gmail.com>
2017-07-17 01:24:33 +0900

    remove build

--- build/GTNavigationBar.build/Debug-iphoneos/GTNavigationBar.build/Script-7689A4569B25EFBE41E8C3CC.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-GTNavigationBar/Pods-GTNavigationBar-resources.sh"
-
--- build/GTNavigationBar.build/Debug-iphoneos/GTNavigationBar.build/Script-E8A35D7F28B8E562665786D7.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-"${SRCROOT}/Pods/Target Support Files/Pods-GTNavigationBar/Pods-GTNavigationBar-frameworks.sh"
-
